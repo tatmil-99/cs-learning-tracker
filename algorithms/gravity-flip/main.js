46a16e3383b48ad47115ff7054e838495a962aa6
Tatien Miller <68832615+tatmil-99@users.noreply.github.com>
2022-03-19 01:56:52 -0500

    adds instructions and algorithm

---
+++ algorithms/gravity-flip/main.js
@@ -0,0 +1,17 @@
+/* Store each columns qty in an object for reference. 
+
+Start at "d" and while less than a.length - 1, for each
+column, add the difference while less than largest "columnQty" prop and 
+subtract the number taken from the respective column(s), then
+iterate and reference the next largest obj prop name. 
+
+Return arr. */
+
+
+const columnQty = {
+
+}
+
+function flip(d, a) {
+  //return 
+}
