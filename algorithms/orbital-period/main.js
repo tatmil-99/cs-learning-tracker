ea87edd66d5ee67e2d11e09f2dd9384899c64881
Tatien Miller <68832615+tatmil-99@users.noreply.github.com>
2022-03-31 03:32:20 -0500

    adds ability to update object propery name with new name and value

--- algorithms/orbital-period/main.js
+++ algorithms/orbital-period/main.js
@@ -1,17 +1,26 @@
 function calculation(altitude) {
   const GM = 398600.4418;
   const earthRadius = 6367.4447;
-  const semiMajorAxis = altitude + earthRadius;
-  
+  let semiMajorAxis = altitude + earthRadius;
+
   return (Math.round(2 * Math.PI * 
     Math.sqrt(Math.pow(semiMajorAxis, 3) / GM)
   ));
 }
 
 function orbitalPeriod(arr) {
-  const altitude = arr[0].avgAlt;
-  const objOrbPeriod = calculation(altitude);
-  console.log(objOrbPeriod); 
+  let altitude = arr[0].avgAlt;
+  let objOrbPeriod = calculation(altitude);
+
+  const transform = arr.map(obj => {
+    return {
+      name : obj.name,
+      orbitalPeriod : obj.avgAlt = objOrbPeriod,
+    };
+  });
+  console.log(transform)
+  return transform;
 }
 
-orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
+//orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
+orbitalPeriod([{name: "iss", avgAlt: 413.6}, {name: "hubble", avgAlt: 556.7}, {name: "moon", avgAlt: 378632.553}])
