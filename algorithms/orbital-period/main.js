cfc4bc474d7227ea38d649bc4d8d5413486ad2ca
Tatien Miller <68832615+tatmil-99@users.noreply.github.com>
2022-03-31 03:01:21 -0500

    adds helper function to calculate orbital period from object altitude

--- algorithms/orbital-period/main.js
+++ algorithms/orbital-period/main.js
@@ -1,16 +1,17 @@
-function calculation(semiMajorAxis, GM) {
+function calculation(altitude) {
+  const GM = 398600.4418;
+  const earthRadius = 6367.4447;
+  const semiMajorAxis = altitude + earthRadius;
+  
   return (Math.round(2 * Math.PI * 
     Math.sqrt(Math.pow(semiMajorAxis, 3) / GM)
   ));
 }
 
 function orbitalPeriod(arr) {
-  const GM = 398600.4418;
-  const earthRadius = 6367.4447;
   const altitude = arr[0].avgAlt;
-  const semiMajorAxis = altitude + earthRadius;
-
-  console.log(calculation(semiMajorAxis, GM));
+  const objOrbPeriod = calculation(altitude);
+  console.log(objOrbPeriod); 
 }
 
 orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
