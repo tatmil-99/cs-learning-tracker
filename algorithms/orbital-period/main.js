f8b013bf7f4cd0948e90917aa2258176483a6e50
Tatien Miller <68832615+tatmil-99@users.noreply.github.com>
2022-03-31 02:53:17 -0500

    updates function

--- algorithms/orbital-period/main.js
+++ algorithms/orbital-period/main.js
@@ -1,14 +1,16 @@
+function calculation(semiMajorAxis, GM) {
+  return (Math.round(2 * Math.PI * 
+    Math.sqrt(Math.pow(semiMajorAxis, 3) / GM)
+  ));
+}
+
 function orbitalPeriod(arr) {
   const GM = 398600.4418;
   const earthRadius = 6367.4447;
   const altitude = arr[0].avgAlt;
   const semiMajorAxis = altitude + earthRadius;
 
-  // Oribital period formula
-  console.log(
-    Math.round(2 * Math.PI * 
-    Math.sqrt(Math.pow(semiMajorAxis, 3) / GM))
-  );
+  console.log(calculation(semiMajorAxis, GM));
 }
 
 orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
