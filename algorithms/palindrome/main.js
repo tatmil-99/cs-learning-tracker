2bfeeb9068c3fe39ef52feefd88b4d657b2d6b2b
Tatien Miller <68832615+tatmil-99@users.noreply.github.com>
2022-03-31 23:20:44 -0500

    adds folder and file to track palindrome algorithm progress

---
+++ algorithms/palindrome/main.js
@@ -0,0 +1,21 @@
+function palindrome(str) {
+  const alphaNum = str
+    .toLowerCase()
+    .split('')
+    .filter(char => {
+      if (/[a-z0-9]/.test(char)) {
+        return char
+      }
+    })
+    .join('');
+
+  console.log(alphaNum)
+}
+
+palindrome("eye");
+palindrome("_eye")
+palindrome("race car")
+palindrome("A man, a plan, a canal. Panama")
+palindrome("My age is 0, 0 si ega ym.")
+palindrome("0_0 (: /-\ :) 0-0")
+palindrome("five|\_/|four")
