751b7147e459e75106e8158abefe5f6ef5dec3b0
Tatien Miller <68832615+tatmil-99@users.noreply.github.com>
2022-04-08 03:21:06 -0500

    updates current status on algoritm. Passes 4 / 6 test cases

--- algorithms/cash-register/main.js
+++ algorithms/cash-register/main.js
@@ -16,43 +16,52 @@ const register = {
 };
 
 function checkCashRegister(price, cash, cid) {
-  let change = cash - price;
+  let change = cash - price; 
 
-  let drawerAmt = cid
+  const cidClone = JSON.parse(JSON.stringify(cid)); 
+
+  const sortedCid = [];
+  for (let i = cidClone.length - 1; i >= 0; i--) {
+    sortedCid.push(cidClone[i]);
+  }
+
+  let drawerAmt = sortedCid
     .reduce((previous, current) => previous.concat(current))
     .filter(element => typeof element === "number")
     .reduce((previous, current) => previous + current);
-
-  const sortedCid = [];
-  for (let i = cid.length - 1; i >= 0; i--) {
-    sortedCid.push(cid[i]);
-  }
   
-  // Use forEach to count back change from cid
+  // Use forEach to count back change from cid and track amount in drawer
   sortedCid.forEach(arr => {
     const amt = 1;
     const unit = 0;
     
-    if (arr[amt] <= change && arr[amt] > 0) {
+    while (
+      arr[amt] <= change && 
+      arr[amt] > 0
+      ) {
       arr[amt] -= currency[arr[unit]];
       drawerAmt -= currency[arr[unit]];
       change -= currency[arr[unit]];
     }
   });
-  
+
   // Check for sufficient funds and update register stats
-  if (change > 0) {
+  if (change.toFixed(2) > 0) {
     register.status = "INSUFFICIENT_FUNDS";
-  } else if (change === drawerAmt) {
+    register.change = []; 
+  } else if (
+    Math.round(change).toFixed(2) === 
+    Math.round(drawerAmt).toFixed(2)
+    ) {
     register.status = "CLOSED";
     register.change = cid;
   }
-
+  
   return register;
 }
 
-// checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+//checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
 
-// checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])
+//checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])
 
 checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])
