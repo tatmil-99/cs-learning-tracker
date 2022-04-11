80aa823b2263c1a2d33994d229835e4419b92c94
Tatien Miller <68832615+tatmil-99@users.noreply.github.com>
2022-04-10 23:57:46 -0500

    finally finishes ffc algorithm projects

--- algorithms/cash-register/main.js
+++ algorithms/cash-register/main.js
@@ -1,61 +1,70 @@
-const currency = {
-  "ONE HUNDRED" : 100,
-  "TWENTY" : 20,
-  "TEN" : 10,
-  "FIVE" : 5, 
-  "ONE" : 1,
-  "QUARTER" : 0.25,
-  "DIME" : 0.1,
-  "NICKEL" : 0.05,
-  "PENNY" : 0.01,
-};
+function checkCashRegister(price, tender, cid) {
+  const currency = {
+    "ONE HUNDRED" : 100,
+    "TWENTY" : 20,
+    "TEN" : 10,
+    "FIVE" : 5, 
+    "ONE" : 1,
+    "QUARTER" : 0.25,
+    "DIME" : 0.1,
+    "NICKEL" : 0.05,
+    "PENNY" : 0.01,
+  };
 
-const register = {
-  status: "",
-  change: []
-};
+  const register = {
+    status: "",
+    change: []
+  };
 
-function checkCashRegister(price, cash, cid) {
-  let change = cash - price; 
+  const cidSorted = [];
   const cidClone = JSON.parse(JSON.stringify(cid)); 
 
-  // Sorts cid from greatest to least to return change like so 
-  const sortedCid = [];
   for (let i = cidClone.length - 1; i >= 0; i--) {
-    sortedCid.push(cidClone[i]);
+    cidSorted.push(cidClone[i]);
   }
 
-  // Calculate total of type number in cid using reduce and filter
-  let drawerAmt = sortedCid
-    .reduce((previous, current) => previous.concat(current))
-    .filter(element => typeof element === "number")
-    .reduce((previous, current) => previous + current);
-  
-  // Use forEach to count back change from cid and track amount in drawer
-  sortedCid.forEach(cash => {
+  let drawerAmt = cidSorted
+    .reduce((previous, current) => {
+      return previous.concat(current)
+    })
+    .filter(element => {
+      return typeof element === "number"
+    })
+    .reduce((previous, current) => {
+      return previous + current
+    });
+
+  let difference = tender - price; 
+
+  cidSorted.forEach(tender => {
     const unit = 0;
     const amt = 1;
-    let total = 0;
-    // Use while loop alter state of cid and track change to return 
+
+    let change = 0;
+
+    // Track state of cid and amt of change to return 
     while (
-      currency[cash[unit]] <= change.toFixed(2) && 
-      cash[amt] > 0   
+      currency[tender[unit]] <= 
+      difference.toFixed(2) && 
+      tender[amt] > 0   
       ) {
-      cash[amt] -= currency[cash[unit]];
-      drawerAmt -= currency[cash[unit]];
-      change -= currency[cash[unit]];
-      // Counts total change to return in each monetary unit
-      total += currency[cash[unit]];
-      register.change.push([cash[unit], total]);
+      tender[amt] -= currency[tender[unit]];
+      drawerAmt -= currency[tender[unit]];
+      difference -= currency[tender[unit]]; 
+      change += currency[tender[unit]];
+    }
+
+    if (change > 0) {
+      register.change.push([tender[unit], change]);
     }
   });
   
   // Check for sufficient funds and update register status
-  if (change.toFixed(2) > 0) {
+  if (difference.toFixed(2) > 0) {
     register.status = "INSUFFICIENT_FUNDS";
     register.change = []; 
   } else if (
-    Math.round(change).toFixed(2) === 
+    Math.round(difference).toFixed(2) === 
     Math.round(drawerAmt).toFixed(2)
     ) {
     register.status = "CLOSED";
@@ -63,15 +72,18 @@ function checkCashRegister(price, cash, cid) {
   } else {
     register.status = "OPEN";
   }
-
-  console.log(register)
+  
   return register;
 }
 
-//checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+//checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) // should return an object.
 
-//checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])
+checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) // should return {status: "OPEN", change: [["QUARTER", 0.5]]}. 
 
-//checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])
+// checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) // should return {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}.
 
-checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
+// checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) // should return {status: "INSUFFICIENT_FUNDS", change: []}.
+
+// checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) // should return {status: "INSUFFICIENT_FUNDS", change: []}.
+
+// checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) // should return {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}.
